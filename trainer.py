ac65fc558bb7da5a08126b5a9ce0e75ffb7eb9f4
yusuf <useful.kyo@gmail.com>
2025-09-20 16:32:32 +0100

    update

--- trainer.py
+++ trainer.py
@@ -259,7 +259,7 @@ def trainer_3d(args, model, snapshot_path):
                         seg_logits, _, _ = model(image_batch)
                         loss_ce = ce_loss(seg_logits, label_batch.long())
                         loss_gdl = gdl_loss(seg_logits, label_batch)
-                        loss = 0.3 * loss_ce + 0.7 * loss_gdl
+                        loss = 0.4 * loss_ce + 0.6 * loss_gdl
 
                     optimizer.zero_grad()
                     scaler.scale(loss).backward()
@@ -303,7 +303,7 @@ def trainer_3d(args, model, snapshot_path):
                 val_dice_history[k].append(metrics[k]["dice"])
 
             # --- Early stopping logic ---
-            min_dice_threshold = 0.430985  # 🔧 adjust as needed
+            min_dice_threshold = 0.230985  # 🔧 adjust as needed
             improved = False
             if mean_dice >= min_dice_threshold and mean_dice > best_performance:
                 best_performance = mean_dice
